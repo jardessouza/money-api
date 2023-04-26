6d0d4dbf216d8388b7728bbb07a78fe70f099fc8
Jardes Souza <jardessouza@gmail.com>
2023-04-26 20:20:24 -0300

    Adicionado configuracoes de bancos de dados h2 e mysql

---
+++ src/test/java/com/jardessouza/moneyapi/MoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jardessouza.moneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
